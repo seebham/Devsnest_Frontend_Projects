cc5fef9cbb2eb9882f91b41f0fcec0994a957cfc
seebham <shubhamisdev@gmail.com>
2021-07-07 20:19:01 +0530

    Day_017 init

---
+++ Day_017/day17/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const AppContainer = styled.div`
+  background-color: white;
+`;
+
+function App() {
+  return <AppContainer></AppContainer>;
+}
+
+export default App;
---
+++ Day_017/day17/src/CalorieCard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const Card = styled.div`
+
+`
+
+const CalorieCard = () => {
+  return <Card></Card>;
+};
+
+export default CalorieCard;
