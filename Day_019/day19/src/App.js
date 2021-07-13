690d2def15cd84b66b70f8cd203b3dbbc59dbf2d
seebham <shubhamisdev@gmail.com>
2021-07-13 13:47:10 +0530

    day 19 finishing

--- Day_019/day19/src/App.js
+++ Day_019/day19/src/App.js
@@ -3,6 +3,13 @@ import Counter from "./Counter";
 
 const AppContainer = styled.main`
   position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 1em;
 `;
 
 function App() {
--- Day_019/day19/src/Counter.jsx
+++ Day_019/day19/src/Counter.jsx
@@ -1,14 +1,27 @@
 import React from "react";
 import styled from "styled-components";
 
-const CounterDiv = styled.div`
-  background-color: #eee;
+const CounterCard = styled.div`
+  width: 2em;
+  height: 2em;
+  cursor: pointer;
+  font-size: 2em;
+  font-weight: 500;
+  background-color: #476072;
+  padding: 0.5em;
+  border-radius: 0.3em;
+  text-align: center;
+  display: grid;
+  place-items: center;
+  font-weight: 900;
 `;
 
 const Counter = () => {
   const [count, setcount] = React.useState(0);
   return (
-    <div onClick={() => setcount((prevCount) => prevCount + 1)}>{count}</div>
+    <CounterCard onClick={() => setcount((prevCount) => prevCount + 1)}>
+      <span>{count}</span>
+    </CounterCard>
   );
 };
 
