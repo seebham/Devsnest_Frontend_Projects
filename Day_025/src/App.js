b9dbabdc57b50770ecf77328452670b385592423
seebham <shubhamisdev@gmail.com>
2021-07-29 20:33:22 +0530

    Day 25

---
+++ Day_025/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-black py-6 flex flex-col justify-center center text-white sm:py-12">
+      <div className="text-2xl border-white">Tailwind</div>
+    </div>
+  );
+}
+
+export default App;
