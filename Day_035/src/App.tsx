a0b930e7697af533467d4682186d59f2a7525436
seebham <shubhamisdev@gmail.com>
2021-08-31 23:11:55 +0530

    THA 35 init

---
+++ Day_035/src/App.tsx
@@ -0,0 +1,9 @@
+import React, { useState, FC } from "react";
+
+const App: FC = () => {
+  const [count, setCount] = useState(0);
+
+  return <div className="bg-red-500">Instagram</div>;
+};
+
+export default App;
