b294eaabff4786d0dcabce011ab4331cf7e8471a
seebham <shubhamisdev@gmail.com>
2021-09-01 22:10:54 +0530

    sparkles ✨

--- Day_035/src/App.tsx
+++ Day_035/src/App.tsx
@@ -1,9 +1,15 @@
 import React, { useState, FC } from "react";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 const App: FC = () => {
-  const [count, setCount] = useState(0);
-
-  return <div className="bg-red-500">Instagram</div>;
+  return (
+    <div className="flex flex-col h-screen">
+      <Header />
+      <main className="flex-1 overflow-y-auto p-5">sda</main>
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
---
+++ Day_035/src/components/Header.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = () => {
+  return <div className="py-1 px-5 max-w-screen-lg md:px-10">Instabuy</div>;
+};
+
+export default Header;
